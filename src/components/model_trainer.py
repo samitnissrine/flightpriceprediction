ca2a4dfb2016d70fc4c401707154f6a09a452020
samitnissrine <nissrine.sam@gmail.com>
2024-06-04 19:10:15 +0200

    model trainer file

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -1,34 +1,76 @@
-from src.exception.exception import customexception
-from src.logger.logging import logging
-import pandas as pd 
-import numpy as np 
 import os
 import sys
+import mlflow
+import mlflow.sklearn
+import numpy as np
+import pickle
+from src.utils.utils import load_object
+from urllib.parse import urlparse
+from sklearn.metrics import mean_squared_error,mean_absolute_error,r2_score
+
+from src.logger.logging import logging
+from src.exception.exception import customexception
 from dataclasses import dataclass
 from pathlib import Path
 from src.utils.utils import save_object,evaluate_model
+from sklearn.linear_model import LinearRegression, Ridge
 from xgboost import XGBRegressor
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.linear_model import LinearRegression, Ridge
 from sklearn.tree import DecisionTreeRegressor
 
 
-
-
-@dataclass
-
+@dataclass 
 class ModelTrainerConfig:
-    pass
-
+    trained_model_file_path = os.path.join('artifacts','model.pkl')
+    
+    
 class ModelTrainer:
     def __init__(self):
-        pass
-        
-
-    def initiate_ModelTrainer(self):
+        self.model_trainer_config = ModelTrainerConfig()
+    
+    def initate_model_training(self,train_array,test_array):
         try:
-            pass
+            logging.info('Splitting Dependent and Independent variables from train and test data')
+            X_train, y_train, X_test, y_test = (
+                train_array[:,:-1],
+                train_array[:,-1],
+                test_array[:,:-1],
+                test_array[:,-1]
+            )
+
+            models = {
+            'LinearRegression': LinearRegression(),
+            'Ridge': Ridge(),
+            'Randomforest': RandomForestRegressor(),
+            'xgboost': XGBRegressor(),
+            'DecisionTree': DecisionTreeRegressor() 
+        }
+            
+            model_report:dict=evaluate_model(X_train,y_train,X_test,y_test,models)
+            print(model_report)
+            print('\n====================================================================================\n')
+            logging.info(f'Model Report : {model_report}')
+
+            # To get best model score from dictionary 
+            best_model_score = max(sorted(model_report.values()))
+
+            best_model_name = list(model_report.keys())[
+                list(model_report.values()).index(best_model_score)
+            ]
+            
+            best_model = models[best_model_name]
+
+            print(f'Best Model Found , Model Name : {best_model_name} , R2 Score : {best_model_score}')
+            print('\n====================================================================================\n')
+            logging.info(f'Best Model Found , Model Name : {best_model_name} , R2 Score : {best_model_score}')
+
+            save_object(
+                 file_path=self.model_trainer_config.trained_model_file_path,
+                 obj=best_model
+            )
+          
+
         except Exception as e:
-            logging.info()
+            logging.info('Exception occured at Model Training')
             raise customexception(e,sys)
 
