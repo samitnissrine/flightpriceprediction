9836139277a8693bcce88e4b0aae967ff61fab8b
samitnissrine <nissrine.sam@gmail.com>
2024-06-04 19:25:13 +0200

    model evaluation file not completed yet

--- src/components/model_evaluation.py
+++ src/components/model_evaluation.py
@@ -8,23 +8,19 @@ from src.utils.utils import load_object
 from urllib.parse import urlparse
 from sklearn.metrics import mean_squared_error,mean_absolute_error,r2_score
 from src.logger.logging import logging
-from src.exception.exception import customexception
-
-
-@dataclass
-
-class ModelEvaluationConfig:
-    pass
+from src.exception. exception import customexception
 
 class ModelEvaluation:
     def __init__(self):
         pass
-        
+    
 
-    def initiate_ModelEvaluation(self):
+    def eval_metrics(self,actual,pred):
+        pass
+
+    def initiate_model_evaluation(self,train_array,test_array):
         try:
             pass
-        except Exception as e:
-            logging.info()
-            raise customexception(e,sys)
 
+        except Exception as e:
+            raise customexception(e,sys)
